187238cfefbca6a64cb43078afab63ca8680109d
zengjianhao <98309632+zengjianhao@users.noreply.github.com>
2022-11-20 18:50:26 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include "color.h"
 
 int main(){
-    std::cout << clr::red     << " red "
-              << clr::yellow  << " yellow "
-              << clr::green   << " green "
-              << clr::cyan    << " cyan "
-              << clr::blue    << " blue "
-              << clr::magenta << " magenta "
-              << clr::grey    << " grey "
-              << clr::white   << " white "
-              << clr::reset   << " reset\n";
-    std::cout << clr::red     << clr::on_cyan    << " red "
-              << clr::yellow  << clr::on_blue    << " yellow "
-              << clr::green   << clr::on_magenta << " green "
-              << clr::cyan    << clr::on_red     << " cyan "
-              << clr::blue    << clr::on_yellow  << " blue "
-              << clr::magenta << clr::on_green   << " magenta "
-              << clr::grey    << clr::on_white   << " grey "
-              << clr::white   << clr::on_grey    << " white "
-              << clr::reset                      << " reset\n";
+    std::cout   << clr::red         << " red "
+                << clr::yellow      << " yellow "
+                << clr::green       << " green "
+                << clr::cyan        << " cyan "
+                << clr::blue        << " blue "
+                << clr::magenta     << " magenta "
+                << clr::grey        << " grey "
+                << clr::white       << " white "
+                << clr::reset       << " reset\n";
+    std::cout   << clr::on_red      << " red "
+                << clr::on_yellow   << " yellow "
+                << clr::on_green    << " green "
+                << clr::on_cyan     << " cyan "
+                << clr::on_blue     << " blue "
+                << clr::on_magenta  << " magenta "
+                << clr::on_grey     << " grey "
+                << clr::on_white    << " white "
+                << clr::reset       << " reset\n";
 }
